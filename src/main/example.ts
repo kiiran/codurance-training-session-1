63b2c00dd751cfd63dc4d017bb07f469ba6f575e
Ciaran Edwards <hello@ciaran.codes>
2021-07-15 10:43:04 +0100

    FizzBuzz exercise
    Co-authored-by: carlosb7 <carlosb7@users.noreply.github.com>

--- src/main/example.ts
+++ src/main/example.ts
@@ -1,5 +1,23 @@
-export class Example {
-    add(a: number, b: number) {
-        return a + b;
+export class FizzBuzz {
+    isMultipleOf(number: number, multiplier: number): boolean {
+        return number % multiplier === 0
+    }
+
+    giveMeANumber(a: number): string {
+        const [isMultipleOf3, isMultipleOf5] = [3, 5].map(n => this.isMultipleOf(a, n))
+
+        if (isMultipleOf3 && isMultipleOf5) {
+            return "fizzbuzz"
+        }
+
+        if (isMultipleOf3) {
+            return "fizz"
+        }
+
+        if (isMultipleOf5) {
+            return "buzz"
+        }
+
+        return a.toString()
     }
 }
--- src/test/example.test.ts
+++ src/test/example.test.ts
@@ -1,8 +1,22 @@
-import {Example} from "../main/example";
+import {FizzBuzz} from "../main/example";
 
-describe('example test', () => {
-    it("should give sum of a + b when adding a and b", () => {
-        let example: Example = new Example();
-        expect(example.add(1, 2)).toBe(3);
+describe('Fizzbuzz', () => {
+    it("returns the number in string", () => {
+        let fizzBuzz: FizzBuzz = new FizzBuzz();
+        expect(fizzBuzz.giveMeANumber(1)).toBe("1");
+    })
+    
+    it("returns fizz if is multiple by 3", () => {
+        let fizzBuzz: FizzBuzz = new FizzBuzz();
+        expect(fizzBuzz.giveMeANumber(3)).toBe("fizz");
+    })
+
+    it("returns buzz if is multiple by 5", () => {
+        let fizzBuzz: FizzBuzz = new FizzBuzz();
+        expect(fizzBuzz.giveMeANumber(5)).toBe("buzz");
+    })
+    it("returns fizzbuzz if is multiple by 5 and 3", () => {
+        let fizzBuzz: FizzBuzz = new FizzBuzz();
+        expect(fizzBuzz.giveMeANumber(15)).toBe("fizzbuzz");
     })
 })
